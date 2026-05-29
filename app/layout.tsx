f0edb1153f5b3a9f9f6172b90618c8c3e7f69142
Mohamed Monir <mohamedmonir0@github.com>
2026-05-30 01:45:28 +0300

    Feat: inject Local Business Plumbing Schema for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 
 export const metadata: Metadata = {
   title: "IDROSHAZLI | Pronto Intervento Idraulico a Milano",
-  description: "Servizi di idraulica, gas, climatizzazione e ristrutturazioni a Milano. Qualità premium 24/7.",
+  description: "Pronto intervento idraulico Milano 24/7. Riparazione caldaie, condizionatori, ricerca perdite d'acqua, impianti gas e ristrutturazioni bagni.",
 };
 
 export default function RootLayout({
@@ -30,6 +30,37 @@ export default function RootLayout({
 
         {/* تذييل الصفحة */}
         <Footer />
+
+        {/* كود الـ Schema الـ God Mode الخاص بالسيو المحلي */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "PlumbingService",
+              "name": "Idroshazli - Pronto Intervento Idraulico Milano",
+              "description": "Pronto intervento idraulico Milano 24/7. Riparazione caldaie, condizionatori, ricerca perdite d'acqua, impianti gas e ristrutturazioni bagni.",
+              "image": "https://www.idroshazli.it/icon.jpg",
+              "telephone": "+393896616549",
+              "url": "https://www.idroshazli.it",
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Milano",
+                "addressRegion": "Lombardia",
+                "addressCountry": "IT"
+              },
+              "priceRange": "€€",
+              "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                  "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+                ],
+                "opens": "00:00",
+                "closes": "23:59"
+              }
+            }),
+          }}
+        />
       </body>
     </html>
   );
